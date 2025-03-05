b8cbb746d8e3322b5c58ce7f79b7cb9bb021d97f
Andrea Laserna <aalaserna@up.edu.ph>
2025-03-06 02:03:47 +0800

    colors added

--- MahawMedicineApp/obj/Debug/net9.0/MahawMedicineApp.AssemblyInfo.cs
+++ MahawMedicineApp/obj/Debug/net9.0/MahawMedicineApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MahawMedicineApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84e6bedefabb56b03faae3c30cdeb6b2aeeb9681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc11ca1ffdbf5ad9e40d55de3b176e823004804")]
 [assembly: System.Reflection.AssemblyProductAttribute("MahawMedicineApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MahawMedicineApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
